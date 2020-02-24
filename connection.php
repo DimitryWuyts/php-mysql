c137adc6af573ecbd7d573e636dd6995378ed335
dimitry <dimiw1337@hotmail.com>
2020-02-24 09:14:07 -0500

    no error so far on my PDO class

--- connection.php
+++ connection.php
@@ -1,16 +1,17 @@
 <?php
 
-function openConnection()
-{
-// Try to figure out what these should be for you
-    $dbhost = "DB_HOST";
-    $dbuser = "DB_USER";
-    $dbpass = "DB_USER_PASSWORD";
-    $db = "DB_NAME";
+//function openConnection()
+//{
+//// Try to figure out what these should be for you
+//    $dbhost = "DB_HOST";
+//$dbuser = "DB_USER";
+//$dbpass = "DB_USER_PASSWORD";
+//$db = "DB_NAME";
+//
+//// Try to understand what happens here
+//$pdo = new PDO('mysql:host=' . $dbhost . 'example.com'';dbname=' . $db, $dbuser, $dbpass);
 
-// Try to understand what happens here
-    $pdo = new PDO('mysql:host=' . $dbhost . 'example.com'';dbname=' . $db, $dbuser, $dbpass);
-
-// Why we do this here
-return $pdo;
-}
+$pdo = new PDO('mysql:host=dimitry@localhost;dbname=database', 'dimitry', 'azerty');
+$statement = $pdo->query("id");
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+echo htmlentities($row['id']);
