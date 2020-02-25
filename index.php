9e7f4dbb7e5ffdfb2c98a0fa939590f57dc69619
dimitry <dimiw1337@hotmail.com>
2020-02-25 07:58:34 -0500

    mvc way

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 include 'model/connection.php';
-include 'view/form.php';
+include 'view/insert.php';
 
 ?>
 
--- view/insert.php
+++ view/insert.php
@@ -1,3 +1,11 @@
+<?php
+declare(strict_types = 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -92,3 +100,4 @@
 
 </body>
 </html>
+
